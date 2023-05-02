2c33bbaaac6585b6369cb71650e508125e1281c0
d.violi <d.violi@eco-mind.eu>
2023-05-02 17:56:22 +0200

    MG - setup board and shuffle card

--- memory-game/src/app/pages/board/board.component.ts
+++ memory-game/src/app/pages/board/board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CardModel} from "../../models/card.model";
+import {CardStatusEnum} from "../../models/card.interface";
+import {ngDebug} from "@angular/cli/src/utilities/environment-options";
 
 @Component({
   selector: 'app-board',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
+  cards: CardModel[] = [];
+
+  imagesName = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6'
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initCardGame();
+  }
+
+  initCardGame(){
+    this.cards = [];
+    this.imagesName.forEach((name) => {
+      const card: CardModel = {
+        imageName: name,
+        state: CardStatusEnum.DEFAULT
+      };
+      // insert two identical cards
+      this.cards.push({ ...card });
+      this.cards.push({ ...card });
+    });
+
+   this.cards = this.shuffleCards(this.cards);
+   console.log(this.cards);
+  }
+
+  private shuffleCards(cards: CardModel[]) {
+    let  size = cards.length;
+    while (size) {
+      const i = Math.floor(Math.random() * size--);
+      [cards[size], cards[i]] = [cards[i], cards[size]];
+    }
+    return cards;
   }
 
 }
